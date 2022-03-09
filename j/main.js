0d5c5f1a9287554bae55c6dd81c5b5499ea9d488
margaretau <aumarg.edu@gmail.com>
2022-03-08 22:39:05 -0800

    as is plus lecture changes

--- j/main.js
+++ j/main.js
@@ -2,7 +2,12 @@
 const lowRider = document.querySelector('.low-rider');
 const leverage = document.querySelector('.leverage');
 
-lowRider.preload = true;
-lowRider.volume = 0.5;
+lowRider.preload = true; //helps to prepare it to play when ready
+lowRider.volume = 0.5; //presets the volume levels on pageload
 
 leverage.volume = 0.5;
+
+document.getElementById('stop').addEventListener('click', (e) => {
+    lowRider.pause();
+    lowRider.currentTime = 0; //sets it back to the beginning after you press stop
+});
